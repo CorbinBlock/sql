9e05f6d62bc69dc2f19666af4a9fd4abcc9f2b0f
corbinblock <williamcorbinblock@gmail.com>
2022-03-07 15:06:07 -0600

    ssis firewall rules

--- sql_server/execute_xp_cmdshell.sql
+++ sql_server/execute_xp_cmdshell.sql
@@ -1,17 +1,19 @@
 declare @dos varchar(800)
 
 /*
-,@filepath varchar(500) = 'T:\TEMP\'
-,@filename varchar(500) ='ALLYMRG_CUST_DETAIL_20220228_02_ADHOC.txt'
+,@filepath varchar(500) = 'C:\tmp\'
 
+set @dos = 'pwsh.exe -c "Get-ACL"'
+set @dos = 'pwsh.exe -c "Get-ChildItem $env:USERPROFILE"'
 set @dos = 'pwsh.exe -c "Get-LastBootTime"'
 set @dos = 'pwsh.exe -c "Get-Creds ad"'
+set @dos = 'pwsh.exe -c "netsh advfirewall firewall add rule name = SQLPort dir = in protocol = tcp action = allow localport = 1433 remoteip = localsubnet profile = DOMAIN"'
 set @dos = 'pwsh.exe -c "C:\Users"'
 set @dos = 'pwsh.exe -c "Set-KeepassConfig "'
 
 */
 
-set @dos = 'pwsh.exe -c "Get-Creds ad"'
+set @dos = 'pwsh.exe -c "netsh advfirewall firewall add rule name = SQLPort dir = in protocol = tcp action = allow localport = 1433 remoteip = localsubnet profile = DOMAIN"'
 
 print @dos
 
